08595d8864516fd5bd22428af14f235b877e5164
denis <denis.lukianov1801@gmail.com>
2025-05-27 01:54:25 +0600

    bugfix: исправлено поле consent_to_processing в сериализаторе профиля

--- backend/users/serializers/user.py
+++ backend/users/serializers/user.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from common.constants.user import LEN_FIRST_NAME
 from users.models.user import UserAccount
 
 
@@ -19,11 +20,22 @@ class UserAccountSerializer(serializers.ModelSerializer):
             'passport_issue_date',
             'passport_issued_by',
             'consent_to_rights',
-            'сonsent_to_processing'
+            'consent_to_processing'
         ]
 
     def create(self, validated_data):
         return UserAccount.objects.create_user(**validated_data)
+
+
+class UserRegistrationSerializer(serializers.Serializer):
+    first_name = serializers.CharField(max_length = LEN_FIRST_NAME)
+    email = serializers.EmailField()
+    password = serializers.CharField(min_length = 8)
+
+    def validate_email(self, email):
+        if UserAccount.objects.filter(email = email).exists():
+            raise serializers.ValidationError('Пользователь с такой электронной почтой уже зарегистрирован.')
+        return email
 
 
 class UserSmallSerializer(serializers.ModelSerializer):
