fe359633809a803ad7d23ffde445b63cc661db72
denis <denis.lukianov1801@gmail.com>
2025-05-27 17:52:53 +0600

    feat: реализована JWT-авторизация пользователей

--- backend/users/urls/auth.py
+++ backend/users/urls/auth.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from users.views.auth import UserRegistrationAPI, LoginView, LogoutView
+from users.views.auth import UserRegistrationAPI, LoginView, LogoutView, AuthorizationAPI, RefreshTokenAPI
 
 urlpatterns = [
     path('user/auth/registration/', UserRegistrationAPI.as_view(), name = 'signup'),
     path('user/auth/login/', LoginView.as_view(), name = 'login'),
-    path('user/auth/logout/', LogoutView.as_view(), name = 'logout')
+    path('user/auth/logout/', LogoutView.as_view(), name = 'logout'),
+
+    path('user/auth/authorization/', AuthorizationAPI.as_view(), name = 'authorization'),
+    path('user/auth/refresh-token/', RefreshTokenAPI.as_view(), name = 'refresh-token'),
 ]
