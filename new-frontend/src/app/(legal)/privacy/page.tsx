ba554f07e18c20b25c1555b1c734f69646bd2340
kairat <k_kurmanov@list.ru>
2025-07-16 15:44:08 +0600

    Удалена страница

--- new-frontend/src/app/(legal)/privacy/page.tsx
+++
@@ -1,8 +0,0 @@
-import policy from '@/shared/legal-json/privacy.json'
-import Documentation from '@/widgets-page/legal/Documentation'
-
-export default async function PrivacyPage() {
-  return (
-        <Documentation data={policy}/>
-  )
-}
